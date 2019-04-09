3bd39c12d75c18abb916793ca8fbcf15ccd5f57a
sghwang <sghwang@enliple.com>
2019-04-09 12:20:05 +0900

    controller 수정

--- src/main/java/com/sghwang/springwebservice/web/WebRestController.java
+++ src/main/java/com/sghwang/springwebservice/web/WebRestController.java
@@ -1,6 +1,11 @@
 package com.sghwang.springwebservice.web;
 
+import com.sghwang.springwebservice.domain.posts.PostsRepository;
+import com.sghwang.springwebservice.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -8,11 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @ResponseBody를 적용시켜준다.
  */
 
+/*
+ * Bean을 주입받는 방법은 1.@Autowired, 2.setter 3.생성자
+ * 이 중에서 생성자로 주입받는 것을 가장 권장.
+ *
+ * @AllArgsConstructor
+ *      - 현재 클래스의 모든 필드를 인자값으로 하는 생성자를 만들어줌.
+ */
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
